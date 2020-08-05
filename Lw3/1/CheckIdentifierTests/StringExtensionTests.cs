b2f08db8ae57249aff1743f5af8e466be5f2f756
unknown <nikolai-4545@mail.ru>
2020-08-05 17:40:04 +0300

    Добавил новые проверки в тесте

--- Lw3/1/CheckIdentifierTests/StringExtensionTests.cs
+++ Lw3/1/CheckIdentifierTests/StringExtensionTests.cs
@@ -3,16 +3,23 @@ using CheckIdentifier;
 
 namespace CheckIdentifierTests
 {
-    public class StringExtensionTests
+    public class StringExtensionTests 
     {
         [SetUp]
         public void Setup()
         {
         }
 
+        [TestCase("A", true)]
+        [TestCase("_", true)]
+        [TestCase("", false)]
+        [TestCase("1", false)]
+        [TestCase("*", false)]
+        [TestCase("AB", true)]
+        [TestCase("A4", true)]
+        [TestCase("1Ñ", false)]
         [TestCase("snake_case", true)]
         [TestCase("1stName", false)]
-        [TestCase("", false)]
         [TestCase("Self-development", false)]
         public void IsIdentifier_snake_case_TrueReturned(string inStr, bool expected) 
         {
